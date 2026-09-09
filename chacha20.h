816e2497daa75a6ea22be99486389cec9c8f0d30
Levi <levivoth77@gmail.com>
2026-09-09 00:15:28 -0500

    Add ctx struct, optimize chacha20 loop

--- chacha20.h
+++ chacha20.h
@@ -1,8 +1,14 @@
-#ifndef CHACHA_H
-#define CHACHA_H
+#ifndef CHACHA20_H
+#define CHACHA20_H
 
 #include <stddef.h>
 #include <stdint.h>
+
+typedef struct Chacha20_ctx {
+    uint32_t key[8];
+    uint32_t counter;
+    uint32_t nonce[3];
+} Chacha20_ctx;
 
 /**
     - The key must have 8 elements.
@@ -12,6 +18,6 @@
     is called.
     - Pointers must not alias.
 */
-void chacha20(const uint32_t* key, uint32_t counter, const uint32_t* nonce, void* out, const size_t out_len);
+void chacha20(Chacha20_ctx* ctx, void* out, const size_t out_len);
 
 #endif
